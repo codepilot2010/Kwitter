9e7d2a12347032ebe545c219550d2843612dbabd
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-07-10 13:33:51 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -52,3 +52,10 @@ function redirecttoroomname(name)
   localStorage.setItem("room_name",name);
   window.location="kwitter_page.html"
 }
+
+function logout()
+{
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location="index.html";
+}
